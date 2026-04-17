ec914fb4e62013bcaa0db902064239794c8d4f48
Doller1616 <abbhishekbbhardwaj@gmail.com>
2026-04-18 01:28:25 +0530

    Done Product Services

---
+++ src/main/java/com/app/ecom/controller/ProductController.java
@@ -0,0 +1,53 @@
+package com.app.ecom.controller;
+
+import com.app.ecom.dto.ProductRequest;
+import com.app.ecom.dto.ProductResponse;
+import com.app.ecom.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        ProductResponse productResponse = productService.createProduct(productRequest);
+        return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductResponse>> searchProduct(@RequestParam String keyword) {
+        List<ProductResponse> productList = productService.searchProducts(keyword);
+        return ResponseEntity.ok(productList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+       boolean isDeleted = productService.deleteProduct(id);
+       return isDeleted ? ResponseEntity.ok("Product Deleted Successfully")
+               : ResponseEntity.noContent().build();
+    }
+
+
+
+}
